993113236d7697bcf1342040a0293aaddfc6e3f2
arjookumar <2009arjoo@gmail.com>
2020-05-13 21:06:49 +0530

    learning concept for services

--- angular-learning/src/app/service-learning/service-learning.component.ts
+++ angular-learning/src/app/service-learning/service-learning.component.ts
@@ -2,10 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-service-learning',
-  templateUrl: './service-learning.component.html',
+  template: `
+    <h2>Employee List:</h2>
+    <ul *ngFor= "let emp of employees">
+      <li>{{emp.id}}. {{emp.name}} - {{emp.age}}</li>
+    </ul>
+  `,
   styleUrls: ['./service-learning.component.css']
 })
 export class ServiceLearningComponent implements OnInit {
+  public employees = [
+    {"id":1,"name":"Anshul","age":28},
+    {"id":2,"name":"Arjoo","age":34},
+    {"id":3,"name":"Mohit","age":25},
+    {"id":4,"name":"Sancheet","age":21}
+  ]
 
   constructor() { }
 
